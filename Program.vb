e88c8fbcfe6349e46ec492bcf2a2f9176d2a4667
liserdarts <liserdarts@yahoo.com>
2018-03-28 11:22:30 -0500

    Changed code to reprodue exception differences

--- Program.vb
+++ Program.vb
@@ -1,24 +1,44 @@
 Module Program
     Sub Main(args As String())
+        If args.Count > 0 Then
+            RunAsClient(args(0))
+            Return
+        End If
+
         Dim Server As New Listener
 
-        Using Client = New IO.Pipes.NamedPipeClientStream(".", Server.PipeName, IO.Pipes.PipeDirection.Out)
-            Client.Connect
-
-            Dim Bytes = Text.Encoding.UTF8.GetBytes("This is a message sent via a named pipe.")
-            Client.Write(Bytes, 0, Bytes.Length)
-        End Using
+        Dim StartInfo As New ProcessStartInfo
+        StartInfo.FileName = "dotnet"
+        StartInfo.Arguments = String.Format("""{0}"" {1}", Reflection.Assembly.GetExecutingAssembly.Location, Server.PipeName)
+        Dim Child = Process.Start(StartInfo)
+        Child.WaitForExit
 
         Server.StopListening
 
-        'Slow down to make sure the background threads have a chance to finish
-        Threading.Thread.Sleep(1000)
-
-#If Debug Then
-        If Debugger.IsAttached Then
+        SyncLock GetType(Console)
+            Console.ForegroundColor = ConsoleColor.Cyan
+            Console.WriteLine("The background exception(s) can take a second. Don't finish too quickly")
             Console.WriteLine("Press any key to continue")
-            Console.ReadKey
-        End If
-#End If
+            Console.ResetColor
+        End SyncLock
+        Console.ReadKey
+    End Sub
+
+    Private Sub RunAsServer()
+        Dim Server As New Listener
+        Server.StopListening
+    End Sub
+
+    Private Sub RunAsClient(PipeName As String)
+        Dim Client As New IO.Pipes.NamedPipeClientStream(".", PipeName, IO.Pipes.PipeDirection.Out)
+        Client.Connect
+
+        Dim Bytes = Text.Encoding.UTF8.GetBytes("This is a message sent via a named pipe.")
+        Client.Write(Bytes, 0, Bytes.Length)
+        
+        'Slow down to make sure the message gets processed
+        Threading.Thread.Sleep(1000)
+        
+        Client.Dispose
     End Sub
 End Module

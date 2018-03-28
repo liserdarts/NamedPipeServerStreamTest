7cd3ff5acc430005cb09775389e0ed510c299587
liserdarts <liserdarts@yahoo.com>
2018-03-28 10:58:51 -0500

    Revert "Changed code to reprodue exception differences"
    This reverts commit 117b3d0b6fe10ddfe71e586d064f255b6c3ee542.

--- Program.vb
+++ Program.vb
@@ -1,50 +1,24 @@
 Module Program
     Sub Main(args As String())
-        If args.Count > 0 Then
-            RunAsClient(args(0))
-            Return
-        End If
-
         Dim Server As New Listener
 
-        Dim StartInfo As New ProcessStartInfo
-        StartInfo.FileName = "dotnet"
-        StartInfo.Arguments = String.Format("""{0}"" {1}", Reflection.Assembly.GetExecutingAssembly.Location, Server.PipeName)
-        Dim Child = Process.Start(StartInfo)
-        Child.WaitForExit
+        Using Client = New IO.Pipes.NamedPipeClientStream(".", Server.PipeName, IO.Pipes.PipeDirection.Out)
+            Client.Connect
+
+            Dim Bytes = Text.Encoding.UTF8.GetBytes("This is a message sent via a named pipe.")
+            Client.Write(Bytes, 0, Bytes.Length)
+        End Using
 
         Server.StopListening
-        
+
         'Slow down to make sure the background threads have a chance to finish
-        'Threading.Thread.Sleep(1000)
+        Threading.Thread.Sleep(1000)
 
 #If Debug Then
-        SyncLock GetType(Console)
-            Console.ForegroundColor = ConsoleColor.Cyan
-            Console.WriteLine("The background exception(s) can take a second. Don't finish too quickly")
+        If Debugger.IsAttached Then
             Console.WriteLine("Press any key to continue")
-            Console.ResetColor
-        End SyncLock
-        Console.ReadKey
-        'End If
+            Console.ReadKey
+        End If
 #End If
-    End Sub
-
-    Private Sub RunAsServer()
-        Dim Server As New Listener
-        Server.StopListening
-    End Sub
-
-    Private Sub RunAsClient(PipeName As String)
-        Dim Client As New IO.Pipes.NamedPipeClientStream(".", PipeName, IO.Pipes.PipeDirection.Out)
-        Client.Connect
-
-        Dim Bytes = Text.Encoding.UTF8.GetBytes("This is a message sent via a named pipe.")
-        Client.Write(Bytes, 0, Bytes.Length)
-        
-        'Slow down to make sure the message gets processed
-        Threading.Thread.Sleep(1000)
-        
-        Client.Dispose
     End Sub
 End Module
